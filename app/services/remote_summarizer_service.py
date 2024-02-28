5c0fa472baa9af3a6c8d7de648827dd35597ac4f
Igor Inozemtsev <inozemtsev.iv@gmail.com>
2024-02-28 10:03:43 +0300

    software engineering 2: homework 1

--- app/services/remote_summarizer_service.py
+++ app/services/remote_summarizer_service.py
@@ -4,16 +4,21 @@ from app.services.base_summarizer_service import BaseSummarizerService
 
 
 class RemoteSummarizerService(BaseSummarizerService):
+    host = "https://api-inference.huggingface.co/"
+    model = "models/IlyaGusev/mbart_ru_sum_gazeta"
+
     def __init__(self, api_token):
         # Подготовка сервиса к работе: сохранение токена и урла модели
         self.api_token = api_token
-        self.api_url = "https://api-inference.huggingface.co/models/IlyaGusev/mbart_ru_sum_gazeta"
+        self.api_url = f"{self.host}{self.model}"
 
     def summarize(self, text):
         headers = {"Authorization": f'Bearer {self.api_token}'}
 
         def query(payload):
-            response = requests.post(self.api_url, headers=headers, json=payload)
+            response = requests.post(self.api_url,
+                                     headers=headers,
+                                     json=payload)
             return response.json()
 
         output = query({
